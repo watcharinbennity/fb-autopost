87bd252bfb69825efd4afb013b69c141a413e949
watcharinbennity <bennity@hotmail.co.th>
2026-03-01 15:27:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 
 PAGE_ID = "110817595401376"
-PAGE_ACCESS_TOKEN = "https://www.facebook.com/profile.php?id=100069823276627"
+PAGE_ACCESS_TOKEN = "EAAMU12USPiwBQ3xEejlecZBmyhsB2O7qFk2pK8g2D2q6KFt8ZCR8KTUoIDcqOBRZBfDWhrZCN1JSXlELmphelf94ZBb5cMtKUqT5mnrNZCMxLXCKlZBFu3sqSZBd4OB6yzth7jZBuSUHvtYbpzq9xZBJcmGZCCA46E0jqjlWrI17LnwvcZCr7MUVqTZCZCkMGPB6WxPohh5LtROSiZCudttZB42jRdspVf6eOaxHsVo4xG4Ns95VqdBwHAVeRSJQx7BdPrp7"
 
 def post_to_facebook(message):
     url = f"https://graph.facebook.com/v18.0/{PAGE_ID}/feed"
