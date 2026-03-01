b22df01ff936b68501d228af4d40c3f35bf60a89
watcharinbennity <bennity@hotmail.co.th>
2026-03-01 15:03:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 
-PAGE_ID = "PUT_PAGE_ID_HERE"
-PAGE_ACCESS_TOKEN = "PUT_PAGE_ACCESS_TOKEN_HERE"
+PAGE_ID = "110817595401376"
+PAGE_ACCESS_TOKEN = "https://www.facebook.com/profile.php?id=100069823276627"
 
 def post_to_facebook(message):
     url = f"https://graph.facebook.com/v18.0/{PAGE_ID}/feed"
