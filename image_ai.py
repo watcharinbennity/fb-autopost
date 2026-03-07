b8b36816d91c2846dad3982ba79561637d92437a
watcharinbennity <bennity@hotmail.co.th>
2026-03-07 17:34:37 +0700

    Create image_ai.py

---
+++ image_ai.py
@@ -0,0 +1,15 @@
+def map_image(text):
+
+ if "โซล่า" in text:
+  return "solar.jpg"
+
+ if "ปลั๊ก" in text:
+  return "safe_plug.jpg"
+
+ if "เครื่องมือ" in text:
+  return "tools.jpg"
+
+ if "LED" in text:
+  return "led_save_power.jpg"
+
+ return "home_electrical_5.jpg"
