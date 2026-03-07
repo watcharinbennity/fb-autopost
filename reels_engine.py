2ef0c736eeb90cbbea67164e431dc7e0fec19520
watcharinbennity <bennity@hotmail.co.th>
2026-03-07 16:37:45 +0700

    Update reels_engine.py

--- reels_engine.py
+++ reels_engine.py
@@ -1,15 +1,5 @@
-from ai_engine import ask_ai
+from ai_engine import reels_script
 
-def reels_script(product):
+def generate_reels(product):
 
-    prompt=f"""
-เขียน script video reel 15 วินาที
-
-สินค้า {product['name']}
-
-Hook
-โชว์สินค้า
-ปิดการขาย
-"""
-
-    return ask_ai(prompt)
+    return reels_script(product)
