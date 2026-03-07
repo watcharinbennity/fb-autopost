e0d635eaa440f01e12fa83a3799c379f5c6840fe
watcharinbennity <bennity@hotmail.co.th>
2026-03-07 10:38:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,33 +9,36 @@ TOKEN = os.getenv("PAGE_ACCESS_TOKEN")
 
 AFF_ID = "15328100363"
 
-STATE_FILE = "state.json"
 PRODUCT_FILE = "products.json"
+STATE_FILE = "state.json"
 
 
 def load_json(file, default):
+
     try:
-        with open(file, "r", encoding="utf-8") as f:
+        with open(file,"r",encoding="utf8") as f:
             return json.load(f)
+
     except:
         return default
 
 
-def save_json(file, data):
-    with open(file, "w", encoding="utf-8") as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
+def save_json(file,data):
+
+    with open(file,"w",encoding="utf8") as f:
+        json.dump(data,f,ensure_ascii=False,indent=2)
 
 
-def convert_affiliate_link(product_url):
+def convert_affiliate(product_link):
 
-    encoded = urllib.parse.quote(product_url)
+    encoded = urllib.parse.quote(product_link)
 
     return f"https://shopee.ee/an_redir?affiliate_id={AFF_ID}&origin_link={encoded}"
 
 
-def caption(p, aff_link):
+def caption(p,link):
 
-    return f"""⚡ แนะนำจาก BEN Home & Electrical
+    text = f"""⚡ แนะนำจาก BEN Home & Electrical
 
 {p['title']}
 
@@ -44,52 +47,54 @@ def caption(p, aff_link):
 💰 ราคา {p['price']} บาท
 
 🛒 สั่งซื้อ
-{aff_link}
+{link}
 
 #BENHomeElectrical #ShopeeAffiliate
 """
 
+    return text
 
-def upload_photo(img):
+
+def upload_photo(image):
 
     url = f"https://graph.facebook.com/v25.0/{PAGE_ID}/photos"
 
-    r = requests.post(url, data={
-        "url": img,
-        "published": "false",
-        "access_token": TOKEN
+    r = requests.post(url,data={
+        "url":image,
+        "published":"false",
+        "access_token":TOKEN
     }).json()
 
     return r["id"]
 
 
-def create_post(media_id, text):
+def create_post(media,text):
 
     url = f"https://graph.facebook.com/v25.0/{PAGE_ID}/feed"
 
-    r = requests.post(url, data={
-        "message": text,
-        "attached_media[0]": json.dumps({"media_fbid": media_id}),
-        "access_token": TOKEN
+    r = requests.post(url,data={
+        "message":text,
+        "attached_media[0]":json.dumps({"media_fbid":media}),
+        "access_token":TOKEN
     }).json()
 
     return r
 
 
-def comment_link(post_id, link):
+def comment_link(post_id,link):
 
     url = f"https://graph.facebook.com/v25.0/{post_id}/comments"
 
-    requests.post(url, data={
-        "message": f"🛒 ลิงก์สินค้า\n{link}",
-        "access_token": TOKEN
+    requests.post(url,data={
+        "message":f"🛒 ลิงก์สินค้า\n{link}",
+        "access_token":TOKEN
     })
 
 
 def main():
 
-    state = load_json(STATE_FILE, {"posted": []})
-    products = load_json(PRODUCT_FILE, [])
+    products = load_json(PRODUCT_FILE,[])
+    state = load_json(STATE_FILE,{"posted":[]})
 
     pool = [p for p in products if p["link"] not in state["posted"]]
 
@@ -99,9 +104,9 @@ def main():
 
     p = random.choice(pool)
 
-    aff_link = convert_affiliate_link(p["link"])
+    aff = convert_affiliate(p["link"])
 
-    text = caption(p, aff_link)
+    text = caption(p,aff)
 
     print("upload image")
 
@@ -109,17 +114,17 @@ def main():
 
     print("create post")
 
-    res = create_post(media, text)
+    res = create_post(media,text)
 
     print(res)
 
     if "id" in res:
 
-        comment_link(res["id"], aff_link)
+        comment_link(res["id"],aff)
 
         state["posted"].append(p["link"])
 
-        save_json(STATE_FILE, state)
+        save_json(STATE_FILE,state)
 
         print("done")
 
