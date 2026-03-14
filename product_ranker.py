715e64bf2b68bd43b1d2ed0a31d2068e499d9ca9
watcharinbennity <bennity@hotmail.co.th>
2026-03-14 16:40:15 +0700

    Update product_ranker.py

--- product_ranker.py
+++ product_ranker.py
@@ -2,8 +2,7 @@ from product_filter import score_product, top_one_percent
 
 
 def rank_products(products):
-    ranked = sorted(products, key=score_product, reverse=True)
-    return ranked
+    return sorted(products, key=score_product, reverse=True)
 
 
 def rank_top_one_percent(products):
