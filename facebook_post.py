d125d2ac4f2c1ed9612e3b7b93ab3b5fffcf6e7e
watcharinbennity <bennity@hotmail.co.th>
2026-03-22 23:59:49 +0700

    Update facebook_post.py

--- facebook_post.py
+++ facebook_post.py
@@ -1,34 +1,29 @@
-import os
 import requests
 from utils import log
 
-PAGE_ID = os.getenv("PAGE_ID", "").strip()
-PAGE_ACCESS_TOKEN = os.getenv("PAGE_ACCESS_TOKEN", "").strip()
-GRAPH_API_VERSION = os.getenv("GRAPH_API_VERSION", "v25.0").strip()
 
-
-def comment(post_id, message):
-    url = f"https://graph.facebook.com/{GRAPH_API_VERSION}/{post_id}/comments"
+def comment(post_id, access_token, message, graph_api_version="v25.0"):
+    url = f"https://graph.facebook.com/{graph_api_version}/{post_id}/comments"
     data = {
         "message": message,
-        "access_token": PAGE_ACCESS_TOKEN
+        "access_token": access_token
     }
     r = requests.post(url, data=data, timeout=60)
     r.raise_for_status()
     log("Comment posted")
 
 
-def post_product(product, caption, comment_text=None):
+def post_product(page_id, access_token, product, caption, comment_text=None, graph_api_version="v25.0"):
     try:
         img = requests.get(product["image"], timeout=120)
         img.raise_for_status()
 
-        url = f"https://graph.facebook.com/{GRAPH_API_VERSION}/{PAGE_ID}/photos"
+        url = f"https://graph.facebook.com/{graph_api_version}/{page_id}/photos"
         files = {"source": ("img.jpg", img.content)}
         data = {
             "caption": caption,
             "published": "true",
-            "access_token": PAGE_ACCESS_TOKEN
+            "access_token": access_token
         }
 
         r = requests.post(url, files=files, data=data, timeout=120)
@@ -38,7 +33,7 @@ def post_product(product, caption, comment_text=None):
         post_id = res.get("post_id") or res.get("id")
 
         if post_id and comment_text:
-            comment(post_id, comment_text)
+            comment(post_id, access_token, comment_text, graph_api_version=graph_api_version)
 
         return post_id
     except Exception as e:
